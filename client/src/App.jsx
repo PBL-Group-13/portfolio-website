606799f3414a868bd1e3f141b9721652062219d0
Mehul Oswal <mehuloswal21@gmail.com>
2021-03-15 19:32:42 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,10 @@
-function App() {
-  const HelloWorld = () => {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-8xl font-bold font-serif animate-pulse text-blue-600">
-          Hello World
-        </h1>
-      </div>
-    );
-  };
-  return (
-    <div className="max-h-screen bg-blue-50">
-      <HelloWorld />
-    </div>
-  );
-}
+// import Login from "./views/Login";
 
-export default App;
+// function App() {
+  
+//   return (
+//     <Login/>
+//   );
+// }
+
+// export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import './index.css';
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import Login from "../src/views/Login.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Redirect from="/" to="/login" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
