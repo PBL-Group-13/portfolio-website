918a3cfbcb2ef584baf2937e1bbb144c0a3180a6
developedBySJ <developedbysj@gmail.com>
2021-05-23 18:37:43 +0530

    fix bug

--- client/src/section/Portfolio/ViewPortfolio/components/Projects.jsx
+++ client/src/section/Portfolio/ViewPortfolio/components/Projects.jsx
@@ -5,68 +5,72 @@ const Projects = ({ projects }) => {
 
   const firstHalf = projects.slice(0, half);
   const secondHalf = projects.slice(-half);
-  console.log({ projects, firstHalf, secondHalf });
+
   return (
     <div class="p-6 sm:p-8 md:p-10 text-justify">
       <h1 class="text-3xl sm:text-4xl font-semibold text-gray-300 mb-6">
         Portfolio
       </h1>
-      <div class="block sm:flex wrap">
-        <div class="w-full sm:w-1/2">
-          {/* copy Start */}
-          {firstHalf.map((fh, i) => {
-            return (
-              <div class="p-2" key={`first-${i}`}>
-                <a href={fh.link} target="blank">
-                  <figure>
-                    <img
-                      src={fh.coverImage}
-                      alt=""
-                      class="w-full h-full object-center object-cover rounded-2xl"
-                    />
-                    <figcaption class="p-2">
-                      <h1 class="text-gray-300 text-lg font-semibold">
-                        {fh.name}
-                      </h1>
-                      <p class="text-gray-500">
-                        {String(fh.description).substring(50)}
-                      </p>
-                    </figcaption>
-                  </figure>
-                </a>
-              </div>
-            );
-          })}
-          {/* copy end */}
+      {projects.length ? (
+        <div class="block sm:flex wrap">
+          <div class="w-full sm:w-1/2">
+            {/* copy Start */}
+            {firstHalf.map((fh, i) => {
+              return (
+                <div class="p-2" key={`first-${i}`}>
+                  <a href={fh.link} target="blank">
+                    <figure>
+                      <img
+                        src={fh.coverImage}
+                        alt=""
+                        class="w-full h-full object-center object-cover rounded-2xl"
+                      />
+                      <figcaption class="p-2">
+                        <h1 class="text-gray-300 text-lg font-semibold">
+                          {fh.name}
+                        </h1>
+                        <p class="text-gray-500">
+                          {String(fh.description).substring(50)}
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </a>
+                </div>
+              );
+            })}
+            {/* copy end */}
+          </div>
+          <div class="w-full sm:w-1/2">
+            {/* copy Start */}
+            {secondHalf.map((fh, i) => {
+              return (
+                <div class="p-2" key={`second-${i}`}>
+                  <a href={fh.link} target="blank">
+                    <figure>
+                      <img
+                        src={fh.coverImage}
+                        alt=""
+                        class="w-full h-full object-center object-cover rounded-2xl"
+                      />
+                      <figcaption class="p-2">
+                        <h1 class="text-gray-300 text-lg font-semibold">
+                          {fh.name}
+                        </h1>
+                        <p class="text-gray-500">
+                          {String(fh.description).substring(50)}
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </a>
+                </div>
+              );
+            })}
+            {/* copy end */}
+          </div>
         </div>
-        <div class="w-full sm:w-1/2">
-          {/* copy Start */}
-          {secondHalf.map((fh, i) => {
-            return (
-              <div class="p-2" key={`second-${i}`}>
-                <a href={fh.link} target="blank">
-                  <figure>
-                    <img
-                      src={fh.coverImage}
-                      alt=""
-                      class="w-full h-full object-center object-cover rounded-2xl"
-                    />
-                    <figcaption class="p-2">
-                      <h1 class="text-gray-300 text-lg font-semibold">
-                        {fh.name}
-                      </h1>
-                      <p class="text-gray-500">
-                        {String(fh.description).substring(50)}
-                      </p>
-                    </figcaption>
-                  </figure>
-                </a>
-              </div>
-            );
-          })}
-          {/* copy end */}
-        </div>
-      </div>
+      ) : (
+        <h1 className="text-2xl text-center mt-4 text-gray-400">No Projects</h1>
+      )}
     </div>
   );
 };
